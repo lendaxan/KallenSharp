9c6f0532f59fa1a9ed43a3add102ca41e4656c80
Kallen Standtfeld <kallenstadtfeldgeass@gmail.com>
2015-11-15 05:38:00 -0800

    Redid Soul Saver
    Shoudl work now

--- S_Plus_Class_Kalista/S_Plus_Class_Kalista/Handlers/SoulBoundHandler.cs
+++ S_Plus_Class_Kalista/S_Plus_Class_Kalista/Handlers/SoulBoundHandler.cs
@@ -16,7 +16,6 @@ namespace S_Plus_Class_Kalista.Handlers
 
         public static void Load()
         {
-            SoulBoundDamage.Load();
             SMenu.AddSubMenu(_Menu());
 
             Game.OnUpdate += OnUpdate;
@@ -26,39 +25,93 @@ namespace S_Plus_Class_Kalista.Handlers
         private static Menu _Menu()
         {
             var menu = new Menu(_MenuNameBase, "souldBoundMenu");
-            menu.AddItem(new MenuItem(_MenuItemBase + "Boolean.AutoSave.IncomingDamage", "Auto-Save soulbound if incoming damage > HP").SetValue(true));
-            menu.AddItem(new MenuItem(_MenuItemBase + "Boolean.AutoSave.Boolean.AutoSavePercent", "Auto-Save soulbound HP%").SetValue(true));
-            menu.AddItem(new MenuItem(_MenuItemBase + "Boolean.AutoSave.Slider.PercentHp", "Auto-Save soulbound when HP% less then").SetValue(new Slider(10, 1, 90)));
+            menu.AddItem(new MenuItem(_MenuItemBase + "Boolean.AutoSave.IncomingDamage", "Auto-Save soulbound if incoming damage >= HP").SetValue(true));
+            menu.AddItem(new MenuItem(_MenuItemBase + "Boolean.AutoSave.IncomingDamage.RemainingHPPercent", ">> max remaining HP% after incming damage").SetValue(new Slider(10, 0, 40)));
+            //menu.AddItem(new MenuItem(_MenuItemBase + "Boolean.AutoSave.Boolean.AutoSavePercent", "Auto-Save soulbound HP%").SetValue(true));
+            //menu.AddItem(new MenuItem(_MenuItemBase + "Boolean.AutoSave.Slider.PercentHp", "Auto-Save soulbound when HP% less then").SetValue(new Slider(10, 1, 90)));
             return menu;
         }
 
+
+        // ReSharper disable once InconsistentNaming
+        private static readonly Dictionary<float, float> _incomingDamage = new Dictionary<float, float>();
+        // ReSharper disable once InconsistentNaming
+        private static readonly Dictionary<float, float> _instantDamage = new Dictionary<float, float>();
+
+        public static float IncomingDamage
+        {
+            get { return _incomingDamage.Sum(e => e.Value) + _instantDamage.Sum(e => e.Value); }
+        }
+
+        private static void OnCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+        {
+            if (!sender.IsEnemy) return;
+            if (SoulBoundHero == null) return;
+            // Calculate Damage
+            if ((!(sender is Obj_AI_Hero) || args.SData.IsAutoAttack()) && args.Target != null && args.Target.NetworkId == SoulBoundHero.NetworkId)
+            {
+                // Calculate arrival time and damage
+                _incomingDamage.Add(SoulBoundHero.ServerPosition.Distance(sender.ServerPosition) / args.SData.MissileSpeed + Game.Time, (float)sender.GetAutoAttackDamage(SoulBoundHero));
+            }
+            // Sender is a hero
+            else if (sender is Obj_AI_Hero)
+            {
+                var attacker = (Obj_AI_Hero)sender;
+                var slot = attacker.GetSpellSlot(args.SData.Name);
+
+                if (slot == SpellSlot.Unknown) return;
+
+                if (slot == attacker.GetSpellSlot("SummonerDot") && args.Target != null && args.Target.NetworkId == SoulBoundHero.NetworkId)
+                    _instantDamage.Add(Game.Time + 2, (float)attacker.GetSummonerSpellDamage(SoulBoundHero, LeagueSharp.Common.Damage.SummonerSpell.Ignite));
+
+                else if (slot.HasFlag(SpellSlot.Q | SpellSlot.W | SpellSlot.E | SpellSlot.R) &&
+                         ((args.Target != null && args.Target.NetworkId == SoulBoundHero.NetworkId) ||
+                          args.End.Distance(SoulBoundHero.ServerPosition) < Math.Pow(args.SData.LineWidth, 2)))
+                    _instantDamage.Add(Game.Time + 2, (float)attacker.GetSpellDamage(SoulBoundHero, slot));
+            }
+        }
         private static void OnUpdate(EventArgs args)
         {
             if (!Limiter.CheckDelay($"{Humanizer.DelayItemBase}Slider.SoulBoundDelay")) return;
 
             Limiter.UseTick($"{Humanizer.DelayItemBase}Slider.SoulBoundDelay");
-            if (SoulBoundHero == null) return;//SoulBoundDamage will bind when able
 
-            if (SoulBoundHero.HealthPercent <
-                SMenu.Item(_MenuItemBase + "Boolean.AutoSave.Slider.PercentHp").GetValue<Slider>().Value
-                 && SoulBoundHero.CountEnemiesInRange(500) > 0 &&
-                 SMenu.Item(_MenuItemBase + "Boolean.AutoSave.Boolean.AutoSavePercent").GetValue<bool>())
+            if (!Champion.R.IsReady()) return;
+
+            if (SoulBoundHero == null)
             {
-                if (Champion.R.Range > SoulBoundHero.Distance(Player) && Champion.R.IsReady())
-                    Champion.R.Cast();
+                SoulBoundHero =
+                    HeroManager.Allies.Find(
+                        ally => ally.Buffs.Any(user => user.Caster.IsMe && user.Name.Contains("kalistacoopstrikeally")));
+                return;
             }
+
+            foreach (var entry in _incomingDamage.Where(entry => entry.Key < Game.Time))
+            {
+                _incomingDamage.Remove(entry.Key);
+            }
+
+            foreach (var entry in _instantDamage.Where(entry => entry.Key < Game.Time))
+            {
+                _instantDamage.Remove(entry.Key);
+            }
+
+
+            //if (SoulBoundHero.HealthPercent <
+            //    SMenu.Item(_MenuItemBase + "Boolean.AutoSave.Slider.PercentHp").GetValue<Slider>().Value
+            //     && SoulBoundHero.CountEnemiesInRange(500) > 0 &&
+            //     SMenu.Item(_MenuItemBase + "Boolean.AutoSave.Boolean.AutoSavePercent").GetValue<bool>())
+            //{
+            //    if (Champion.R.Range > SoulBoundHero.Distance(Player) && Champion.R.IsReady())
+            //        Champion.R.Cast();
+            //}
 
             if (SMenu.Item(_MenuItemBase + "Boolean.AutoSave.IncomingDamage").GetValue<bool>())
             {
-                if (SoulBoundDamage.IncomingDamage > SoulBoundHero.Health)
-                    if (Champion.R.Range > SoulBoundHero.Distance(Player) && Champion.R.IsReady())
+                if (SoulBoundHero.Distance(Player) > Champion.R.Range) return;
+                if (IncomingDamage > SoulBoundHero.Health || IncomingDamage > SoulBoundHero.Health * (100 - SMenu.Item(_MenuItemBase + "Boolean.AutoSave.Boolean.AutoSavePercent").GetValue<Slider>().Value) / 100)
                         Champion.R.Cast();
             }
-
-        }
-
-        private static void OnCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
-        {
 
         }
     }
